b65835854d6abde78512ba14fba657f49f675188
CorianderSeb <106112163+CorianderSeb@users.noreply.github.com>
2023-01-25 02:30:50 -0500

    added search function

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,18 +1,38 @@
-import './Dictionary';
+import Dictionary from './Dictionary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      <h1>Dictionary</h1>
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer>Coded by <a href="www.sebastiancseithel.com" taget="_blank" rel="noopener norefferer">Sebastian Coriander Seithel</a></footer>
-    </div>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            {" "}
+            Coded by{" "}
+            <a
+              href="www.sebastiancseithel.com"
+              taget="_blank"
+              rel="noopener norefferer"
+            >
+              Sebastian Coriander Seithel
+            </a>
+            and{" "}
+            <a
+              href="https://github.com/CorianderSeb/dictionary-app"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              open sourced on GitHub.
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
