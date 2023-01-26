bed76fea8c020737818ff8d5be11f4113c23443f
CorianderSeb <106112163+CorianderSeb@users.noreply.github.com>
2023-01-26 10:07:59 -0500

    trying to fix mistake

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
         "ythPBY38JMeKu1KkGZMTAvPRXszmWNBnSPuvsVtOqLiu4CmPw8bz4S5x";
       let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
       let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-      axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+      axios.get(pexelsApiUrl, { headers: "headers" }).then(handlePexelsResponse);
     }
     
 
